02a7d73e49cdf657decc946601722e5586812e8e
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2021-12-28 15:11:32 +0700

    edit 3

--- microwave.rb
+++ microwave.rb
@@ -4,35 +4,35 @@ class Microwave
         @time = time
     end
     def timer
-        l = @time.length
+        l = @time.to_s.length
         if l <= 2     
               key = @time.to_i
             if key <= 9
-                puts "'00:0#{@time}'"
+                return "'00:0#{@time}'"
             elsif  key > 9 && key < 60
-                puts "'00:#{@time}'"
+                return "'00:#{@time}'"
             elsif  key == 60
-                puts "'01:00'"
+                return "'01:00'"
             else
-                puts "'01:#{@time.to_i-60}'"
+                return "'01:#{@time.to_i-60}'"
             end
         else  
             case  l
             when 4
                 if @time[2,3].to_i > 60
-                    puts "'#{@time[0].to_i+1}:#{@time[1,2].to_i-60}'"
+                    return "'#{@time[0].to_i+1}:#{@time[1,2].to_i-60}'"
                 else
-                    puts "'#{@time[0,1]}:#{@time[2,3]}}'"
+                    return "'#{@time[0,1]}:#{@time[2,3]}}'"
                 end
             else 
                 if @time[1,2].to_i > 60
                     if @time[0].to_i+1 < 10
-                        puts "'0#{@time[0].to_i+1}:#{@time[1,2].to_i-60}'"
+                        return "'0#{@time[0].to_i+1}:#{@time[1,2].to_i-60}'"
                     else
-                        puts "'#{@time[0].to_i+1}:#{@time[1,2].to_i-60}'"
+                        return "'#{@time[0].to_i+1}:#{@time[1,2].to_i-60}'"
                     end
                 else
-                    puts "'0#{@time[0]}:#{@time[1,2]}}'"
+                    return "'0#{@time[0]}:#{@time[1,2]}}'"
                 end  
             end
         end
@@ -40,5 +40,4 @@ class Microwave
     end
    
 end
-m =Microwave.new("120")
-m.timer
+puts Microwave.new(1).timer 
